aff6a854817462faf73d1a5450f29cb2b861e061
nishman89 <53156243+nishman89@users.noreply.github.com>
2022-07-19 14:42:01 +0100

    Updated stuff

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+global::System.Console.WriteLine("Goodbye world :(");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Eng120
+
+I recommend using the application TypeDown - google it
